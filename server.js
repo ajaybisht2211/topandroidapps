d021331fa0a88182818a4a928475417dee39b647
ajay <ajay@vujadelabs.com>
2020-04-02 13:16:04 +0530

    Connect database and save the scrap data in the db

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+var gplay = require('google-play-scraper');
+
+var applistFunction = function (callback) {
+	gplay.list({
+    collection: gplay.collection.TOP_FREE,
+    // num: 2
+  })
+  .then(function(res){
+  	// console.log("-->", res);
+  	callback(res);
+  })
+  // (console.log, console.log);
+}
+
+
 const MongoClient = require('mongodb').MongoClient;
 
 var db;
@@ -13,6 +28,18 @@ MongoClient.connect(DB_URL, (err, client) => {
   if (err) return console.log(err)
   db = client.db('topappsdb') // whatever your database name is
   app.listen(port, () => console.log(`Magic port ${port}!`))
+
+  applistFunction(function(data){
+  	db.collection('androidapp').insertMany(data, (err, result) => {
+	    if (err) return console.log(err)
+
+	    console.log('saved to database');
+	  })
+  });
+
+  // app.post('/quotes', (req, res) => {
+  
+// })
 })
 
 
